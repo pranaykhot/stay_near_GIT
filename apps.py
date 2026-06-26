dbdeb7ac1854dd321791ca092dd28a0d1347f24c
pranay_khot <khotpranay5@gmail.com>
2026-06-26 15:00:24 +0530

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,8 +1,14 @@
 from django.apps import AppConfig
+from django.core import serializers
 from django.utils.translation import gettext_lazy as _
 
 
-class FlatPagesConfig(AppConfig):
+class GISConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "django.contrib.flatpages"
-    verbose_name = _("Flat Pages")
+    name = "django.contrib.gis"
+    verbose_name = _("GIS")
+
+    def ready(self):
+        serializers.BUILTIN_SERIALIZERS.setdefault(
+            "geojson", "django.contrib.gis.serializers.geojson"
+        )
--- views.py
+++ views.py
@@ -1,70 +1,22 @@
-from django.conf import settings
-from django.contrib.flatpages.models import FlatPage
-from django.contrib.sites.shortcuts import get_current_site
-from django.http import Http404, HttpResponse, HttpResponsePermanentRedirect
-from django.shortcuts import get_object_or_404
-from django.template import loader
-from django.utils.safestring import mark_safe
-from django.views.decorators.csrf import csrf_protect
-
-DEFAULT_TEMPLATE = "flatpages/default.html"
-
-# This view is called from FlatpageFallbackMiddleware.process_response
-# when a 404 is raised, which often means CsrfViewMiddleware.process_view
-# has not been called even if CsrfViewMiddleware is installed. So we need
-# to use @csrf_protect, in case the template needs {% csrf_token %}.
-# However, we can't just wrap this view; if no matching flatpage exists,
-# or a redirect is required for authentication, the 404 needs to be returned
-# without any CSRF checks. Therefore, we only
-# CSRF protect the internal implementation.
+from django.http import Http404
+from django.utils.translation import gettext as _
 
 
-def flatpage(request, url):
-    """
-    Public interface to the flat page view.
+def feed(request, url, feed_dict=None):
+    """Provided for backwards compatibility."""
+    if not feed_dict:
+        raise Http404(_("No feeds are registered."))
 
-    Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
-    """
-    if not url.startswith("/"):
-        url = "/" + url
-    site_id = get_current_site(request).id
+    slug = url.partition("/")[0]
     try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith("/") and settings.APPEND_SLASH:
-            url += "/"
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect("%s/" % request.path)
-        else:
-            raise
-    return render_flatpage(request, f)
+        f = feed_dict[slug]
+    except KeyError:
+        raise Http404(_("Slug %r isn’t registered.") % slug)
 
-
-@csrf_protect
-def render_flatpage(request, f):
-    """
-    Internal interface to the flat page view.
-    """
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
-
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
-
-    # To avoid having to always use the "|safe" filter in flatpage templates,
-    # mark the title and content as already safe (since they are raw HTML
-    # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
-
-    return HttpResponse(template.render({"flatpage": f}, request))
+    instance = f()
+    instance.feed_url = getattr(f, "feed_url", None) or request.path
+    instance.title_template = f.title_template or ("feeds/%s_title.html" % slug)
+    instance.description_template = f.description_template or (
+        "feeds/%s_description.html" % slug
+    )
+    return instance(request)
